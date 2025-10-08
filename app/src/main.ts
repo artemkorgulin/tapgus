3f0ff34cf4cd749a7432ffc06c16392ad69dbdd6
Артем Коргулин <a.korgulin@dats.team>
2025-10-08 17:50:22 +0300

    first commit

--- app/src/main.ts
+++ app/src/main.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // Сообщим приложению, где искать наши views.
-  app.setBaseViewsDir(join(__dirname, '../server/views'));
+  app.setBaseViewsDir(join(__dirname, '../views'));
   // И укажем, какой шаблонизатор использовать
   app.setViewEngine('pug');
   app.useGlobalPipes(
