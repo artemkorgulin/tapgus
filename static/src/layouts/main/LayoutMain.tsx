8bb56d1d31f83739b01a50be7c1f8029589d372f
Артем Коргулин <a.korgulin@dats.team>
2025-10-09 15:36:40 +0300

    admin section commit

--- static/src/layouts/main/LayoutMain.tsx
+++ static/src/layouts/main/LayoutMain.tsx
@@ -14,78 +14,87 @@ export const LayoutMain: TComponent<PropsWithChildren> = ({ children }) => {
     const textRef = useRef<null | any>(null);
     const [iUserPoints, setiUserPoints] = useState(0);
 
-    const tapGus = (tap: any) => {
-        if (iUserPoints >= 0 && tap) {
-            try {
-                let token = getToken();
-                let UserPoints = iUserPoints+1;
-                fetch(
-                    `${ENV_BASE_LEGACY_API_URL}/user/tapguss`,
-                    {
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        method: 'POST',
-                        body: JSON.stringify({token, UserPoints}),
-                        mode: 'cors',
-                    }).then((data :any) => {
+    if(getToken()) {
+        const tapGus = (tap: any) => {
+            if (iUserPoints >= 0 && tap) {
+                try {
+                    let token = getToken();
+                    let UserPoints = iUserPoints+1;
+                    fetch(
+                        `${ENV_BASE_LEGACY_API_URL}/user/tapguss`,
+                        {
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            method: 'POST',
+                            body: JSON.stringify({token, UserPoints}),
+                            mode: 'cors',
+                        }).then((data :any) => {
                         if(data) {
                             setiUserPoints(UserPoints);
                         }
                     });
-                return true;
-            } catch (e) {
-                console.error(e);
-                return false;
+                    return true;
+                } catch (e) {
+                    console.error(e);
+                    return false;
+                }
             }
-        }
-    };
+        };
 
-    function handlerTap(event: any) {
-        var elem = event.target;
-        elem.setAttribute(event.type, 1);
-        if (event.type === 'touchend' && !elem.getAttribute('touchmove')) {
-            event.type = 'tap';
-        } else if (elem.getAttribute('touchend')) {
-            event.type = '';
-        }
-        tapGus(event.type);
-    }
-
-    useEffect(() => {
-        if(textRef.current != null){
-            if(textRef.current.textContent ==""){
-                textRef.current.textContent = 0;
+        function handlerTap(event: any) {
+            var elem = event.target;
+            elem.setAttribute(event.type, 1);
+            if (event.type === 'touchend' && !elem.getAttribute('touchmove')) {
+                event.type = 'tap';
+            } else if (elem.getAttribute('touchend')) {
+                event.type = '';
             }
+            tapGus(event.type);
         }
-    }, []);
 
-    return (
-        <ViewerDataContext.Provider value={userData}>
-            <div className={css.main_layout}>
-                <div  className={css.wrapper_user}>
-                    <div className={css.head_user}>
-                        <h2> Welcome to Tap Guss! </h2>
-                    </div>
-                    <div className={css.pic_user}>
-                        <img
-                            onClick={(e) => handlerTap(e)}
-                            onTouchStart={(e) => handlerTap(e)}
-                            onTouchEnd={(e) => handlerTap(e)}
-                            onTouchMove={(e) => handlerTap(e)}
-                            src={logoGus}
-                            alt='logoGus'
-                            className='min-w-max'
-                            width={320}
-                            height={320}
-                        />
-                    </div>
-                    <div className={css.points_user}>
-                        <h2 className={"points-user-head"} id={"points-user-head"} ref={textRef}>{iUserPoints}</h2>
-                        {children}
+        useEffect(() => {
+            if(textRef.current != null){
+                if(textRef.current.textContent ==""){
+                    textRef.current.textContent = 0;
+                }
+            }
+        }, []);
+
+        return (
+            <ViewerDataContext.Provider value={userData}>
+                <div className={css.main_layout}>
+                    <div  className={css.wrapper_user}>
+                        <div className={css.head_user}>
+                            <h2> Welcome to Tap Guss! </h2>
+                        </div>
+                        <div className={css.pic_user}>
+                            <img
+                                onClick={(e) => handlerTap(e)}
+                                onTouchStart={(e) => handlerTap(e)}
+                                onTouchEnd={(e) => handlerTap(e)}
+                                onTouchMove={(e) => handlerTap(e)}
+                                src={logoGus}
+                                alt='logoGus'
+                                className='min-w-max'
+                                width={320}
+                                height={320}
+                            />
+                        </div>
+                        <div className={css.points_user}>
+                            <h2 className={"points-user-head"} id={"points-user-head"} ref={textRef}>{iUserPoints}</h2>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </ViewerDataContext.Provider>
-    );
+            </ViewerDataContext.Provider>
+        );
+    } else {
+        return (
+            <ViewerDataContext.Provider value={userData}>
+                <div className={css.main_layout}>
+                    {children}
+                </div>
+            </ViewerDataContext.Provider>
+        );
+    }
 };
