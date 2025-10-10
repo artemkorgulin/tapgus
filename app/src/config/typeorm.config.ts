dcb5143425bc5f07c692ee4b351973d4efbe9639
Артем Коргулин <a.korgulin@dats.team>
2025-10-10 15:04:16 +0300

    add seeds db

--- app/src/config/typeorm.config.ts
+++ app/src/config/typeorm.config.ts
@@ -4,6 +4,8 @@ import {
     TypeOrmModuleOptions,
 } from '@nestjs/typeorm';
 import {config as dotenvConfig} from "dotenv";
+import * as path from 'path';
+import { DataSource } from 'typeorm';
 
 dotenvConfig({ path: '.env' });
 
@@ -39,3 +41,21 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     synchronize: true,
     logging: false,
 };
+
+export const config = new DataSource({
+    type: 'postgres',
+    host: process.env.TYPEORM_HOST,
+    port: Number(process.env.TYPEORM_PORT) || 5432,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    entities: [
+        path.resolve(`${__dirname}/../../**/**.entity{.ts,.js}`)
+    ],
+    migrations: [
+        path.resolve(`${__dirname}/../../../database/seeders/*{.ts,.js}`)
+    ],
+    migrationsTableName: 'seeders',
+    logging: true,
+    synchronize: false
+});
--- app/static/dto/loginResponse.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LoginResponseDto = void 0;
-var LoginResponseDto = /** @class */ (function () {
-    function LoginResponseDto() {
-    }
-    return LoginResponseDto;
-}());
-exports.LoginResponseDto = LoginResponseDto;
