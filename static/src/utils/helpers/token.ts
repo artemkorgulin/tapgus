53fe5b01727a9509e79b23631a121ab86cd5d38c
Артем Коргулин <a.korgulin@dats.team>
2025-11-15 19:23:50 +0300

    add jwsonwebtoken package

--- static/src/utils/helpers/token.ts
+++ static/src/utils/helpers/token.ts
@@ -1,12 +1,9 @@
-import jwt, { Secret } from 'jsonwebtoken';
-import { ENV_PRIVATE_KEY } from 'app-env';
-export const SECRET_KEY: Secret = String(ENV_PRIVATE_KEY);
+import {jwtDecode} from "jwt-decode";
 
 export const authDecode = async (token: string) => {
     if (!token) {
         throw new Error(`token not decoded`);
     }
 
-    const decoded: any = jwt.verify(token, SECRET_KEY);
-    return decoded;
+    return jwtDecode(token);
 };
